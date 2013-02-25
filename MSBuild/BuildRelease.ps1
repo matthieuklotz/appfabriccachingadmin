b37e85f98ab337ae5150516dc8b0df427821d82d
Matthieu Klotz <matthieu.klotz@mattk.info>
2013-02-25 16:44:54 +0100

    Add build script for release configuration

---
+++ MSBuild/BuildRelease.ps1
@@ -0,0 +1,4 @@
+﻿$solutionfile = "..\AppFabric.Admin.sln"
+$msbuild = $env:windir + "\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+$msbuildargs = $solutionfile + " /t:rebuild /property:Configuration=Release /fl /flp:logfile=releasebuild.log"
+Start-Process $msbuild $msbuildargs
